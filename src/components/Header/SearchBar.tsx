47990e348818aeb80c91605e73652033aabb3c4c
Luan Ramalho <luanfswd@gmail.com>
2022-07-31 01:45:39 -0300

    input search

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -1,11 +1,26 @@
 import './styles/SearchBar.css';
+import { FaSistrix } from 'react-icons/fa';
 import logo from '../../assets/images/Mercado-Livre-logo.png';
 
 const SearchBar = () => {
   return (
     <div className="all-header search-bar">
       <img className="logo-meli" src={logo} alt="logo-meli" />
-      {/* https://seeklogo.com/images/M/mercado-livre-logo-D1DC52B13E-seeklogo.com.png */}
+      <form id="search-form" action="GET">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Buscar produtos, marcas e muito mais..."
+        />
+        <button className="search-btn">
+          <FaSistrix className="search-icon" />
+        </button>
+      </form>
+      <img
+        className="logo-disney"
+        src="https://http2.mlstatic.com/D_NQ_806934-MLA50801953236_072022-OO.webp"
+        alt=""
+      />
     </div>
   );
 };
